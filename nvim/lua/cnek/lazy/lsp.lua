22ff8166884971fa70cca2b822eac05518e321a0
viktorsynek <mail.viktorsynek@gmail.com>
2026-03-01 13:46:36 +0100

    fix annoying on_attach error

--- nvim/lua/cnek/lazy/lsp.lua
+++ nvim/lua/cnek/lazy/lsp.lua
@@ -55,13 +55,9 @@ return {
         local lspconfig = require("lspconfig")
         local util = require("lspconfig.util")
 
-        local base = require("lspconfig")
-        local on_attach = base.on_attach
-
         lspconfig.clangd.setup({
             on_attach = function(client, bufnr)
                 client.server_capabilities.signatureHelpProvider = false
-                on_attach(client, bufnr)
             end,
             capabilities = capabilities,
         })
